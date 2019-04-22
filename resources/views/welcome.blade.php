a52a63e8c8bf385e5ab6f857acb21a8e3a19ddf9
mint <mint@beksong1234567890@gmail.com>
2019-04-22 11:43:21 +0800

    nineth commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,92 +5,122 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>BPSDM Prov. Sulteng</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
+        <nav class="navbar navbar-inverse navbar-fixed-top">
+            <div class="container-fluid">
+                <!-- header -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#main-navbar" aria-expanded="false">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">
+                        <img src="{{ url('storage/profile/LOGO_SULAWESI_TENGAH.png') }}" alt="logo-sulteng" style="width:20px; height:30px;" class="img-responsive"></a>
                 </div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                <!-- container menu on navbar -->
+                <div class="collapse navbar-collapse" id="main-navbar">
+                    <ul class="nav navbar-nav">
+                        <li><a href="#"> BPSDM SULAWESI TENGAH</a></li>
+                    </ul>
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="{{ route('login')}}"> Masuk</a></li>
+                        <li><a href="{{ route('register')}}"> Daftar</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <!-- content -->
+        <div class="row">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                <div id="carousel1" class="carousel slide" data-ride="carousel">
+                    <ol class="carousel-indicators">
+                        <li data-target="carousel1" data-slide-to="0" class="active"></li>
+                        <li data-target="carousel1" data-slide-to="1" class="active"></li>
+                        <li data-target="carousel1" data-slide-to="2" class="active"></li>
+                    </ol>
+
+                    <!-- carousel content -->
+                    <div class="carousel-inner" role="listbox">
+                        <div class="item active">
+                            <img src="{{ url('images/banner1.jpg') }}" alt="banner1" class="img-responsive">
+                            <div class="carousel-caption">
+                                <h3>DATA MENCERDASKAN BANGSA</h3>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <img src="{{ url('images/banner2.jpg') }}" alt="banner1" class="img-responsive">
+                            <div class="carousel-caption">
+                                <h3>DIGITALISASI DATA UNTUK ADMINISTRASI YANG LEBIH BAIK</h3>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <img src="{{ url('images/banner3.jpg') }}" alt="banner1" class="img-responsive">
+                            <div class="carousel-caption">
+                                <h3>SISTEM INFORMASI DIKLAT</h3>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Control -->
+                    <a class="left carousel-control" href="#carousel1" role="button" data-slide="prev">
+                        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="right carousel-control" href="#carousel1" role="button" data-slide="next">
+                        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
                 </div>
             </div>
         </div>
+
+        <div class="row">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                <div class="page-header text-center">
+                    <i><h3>Historical Data</h3></i>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+                <div class="thumbnail text-center">
+                    <div class="caption">
+                        <h3>Alumni</h3>
+                        <span style="font-size:5em;" class="glyphicon glyphicon-education"></span>
+                        <h4>10000+ alumni ASN</h4>
+                        <p>Kami telah mengahasilkan alumni asn yang siap mengabdi demi kemajuan Sulawesi Tengah</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+                <div class="thumbnail text-center">
+                    <div class="caption">
+                        <h3>Pelatihan</h3>
+                        <span style="font-size:5em;" class="glyphicon glyphicon-wrench"></span>
+                        <h4>10000+ Pelatihan ASN</h4>
+                        <p>Melalui berbagai kegiatan pelatihan yang disesuaikan dengan kebutuhan OPD dan perkembangan jaman</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+                <div class="thumbnail text-center">
+                    <div class="caption">
+                        <h3>Kurikulum</h3>
+                        <span style="font-size:5em;" class="glyphicon glyphicon-bookmark"></span>
+                        <h4>Uptodate</h4>
+                        <p>Kurikulum yang menyesuaikan perkembangan dinamika bangsa serta tetap menjunjung nilai-nilai luhur bangsa</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <script src="{{ asset('js/app.js') }}"></script>
     </body>
 </html>
